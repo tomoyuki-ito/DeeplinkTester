c1d93b2adc079c8457cea11d088ea62403325c36
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-03-23 11:59:46 +0900

    feat: change app icon

--- App.js
+++ App.js
@@ -21,3 +21,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// AppIcon
+// https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html#foreground.type=text&foreground.text.text=D&foreground.text.font=Aguafina%20Script&foreground.space.trim=1&foreground.space.pad=0.2&foreColor=rgb(255%2C%20255%2C%20255)&backColor=rgb(68%2C%20138%2C%20255)&crop=0&backgroundShape=square&effects=none&name=icon
