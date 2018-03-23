bf49ab3609e93808e0f523f1dcc3e8916325a09f
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-03-23 16:41:56 +0900

    feat: switchable stage and production

--- app/components/screens/MainScreen.js
+++ app/components/screens/MainScreen.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
-import { Container, Header, Body, Title } from 'native-base'
+import { Container, Header, Left, Body, Right, Button, Text, Title } from 'native-base'
 import { MainList } from '../lists/MainList'
-import { listdata } from '../../listdata/mangajam'
+import { createListdata } from '../../listdata/mangajam'
 
 export default class MainScreen extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      env: 0
+    }
+  }
+
   render() {
     return (
       <Container>
         <Header>
+          <Left>
+          </Left>
           <Body>
             <Title>Deeplink Tester</Title>
           </Body>
+          <Right>
+            <Button
+              transparent
+              onPress={() => {
+                if (this.state.env == 0) {
+                  this.setState({...this.state, env: 1})
+                } else {
+                  this.setState({...this.state, env: 0})
+                }
+              }}
+            >
+              <Text>{this.state.env == 0 ? 'STG' : 'PRD'}</Text>
+            </Button>
+          </Right>
         </Header>
         <MainList
-          data={listdata}
+          data={createListdata(this.state.env)}
         />
       </Container>
     )
   }
+
 }
--- app/listdata/mangajam.js
+++ app/listdata/mangajam.js
@@ -1,48 +1,50 @@
-const domain = 'mangajam'
+export const createListdata = (env) => {
+  const domain = env == 0 ? 'mangajam-adhoc' : 'mangajam'
 
-const createURL = (path) => {
-  return `${domain}://${path}`
+  const createURL = (path) => {
+    return `${domain}://${path}`
+  }
+
+  return [
+    {
+      title: 'ホーム',
+      url: createURL('home'),
+    },
+    {
+      title: '連載',
+      url: createURL('serial'),
+    },
+    {
+      title: '人気',
+      url: createURL('popular'),
+    },
+    {
+      title: '雑誌',
+      url: createURL('magazine'),
+    },
+    {
+      title: '設定',
+      url: createURL('setting'),
+    },
+    {
+      title: '課金',
+      url: createURL('coincharge'),
+    },
+    {
+      title: 'ホームタブ → 作品詳細',
+      url: createURL('home/comicdetail/comicid=1831'),
+    },
+    {
+      title: '人気タブ → ランキング',
+      url: createURL('popular/ranking'),
+    },
+    {
+      title: 'ホームタブ → 読書中',
+      url: createURL('home/reading'),
+    },
+    {
+      title: 'ホームタブ → 特集',
+      url: createURL('home/comicdetail/featureid=1'),
+    },
+  ]
 }
-
-export const listdata = [
-  {
-    title: 'ホーム',
-    url: createURL('home'),
-  },
-  {
-    title: '連載',
-    url: createURL('serial'),
-  },
-  {
-    title: '人気',
-    url: createURL('popular'),
-  },
-  {
-    title: '雑誌',
-    url: createURL('magazine'),
-  },
-  {
-    title: '設定',
-    url: createURL('setting'),
-  },
-  {
-    title: '課金',
-    url: createURL('coincharge'),
-  },
-  {
-    title: 'ホームタブ → 作品詳細',
-    url: createURL('home/comicid=1831'),
-  },
-  {
-    title: '人気タブ → ランキング',
-    url: createURL('popular/ranking'),
-  },
-  {
-    title: 'ホームタブ → 読書中',
-    url: createURL('home/reading'),
-  },
-  {
-    title: 'ホームタブ → 特集',
-    url: createURL('home/featureid=1'),
-  },
-]
