adbd1e64b317737926e59a33901c95822c0ff8c9
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-03-23 11:27:51 +0900

    feat: add react-native-elements

---
+++ app/components/ListScreen.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { ListView } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  }
+]
+
+export default class ListScreen extends React.Component {
+  constructor() {
+    super()
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    })
+
+    this.state = {
+      dataSource: ds.cloneWithRows(list),
+    }
+
+    this.renderRow = this.renderRow.bind(this)
+  }
+
+  renderRow (rowData, sectionID) {
+    return (
+      <ListItem
+        roundAvatar
+        key={sectionID}
+        title={rowData.name}
+        subtitle={rowData.subtitle}
+        avatar={{uri:rowData.avatar_url}}
+      />
+    )
+  }
+
+  render () {
+    return (
+      <List>
+        <ListView
+          renderRow={this.renderRow}
+          dataSource={this.state.dataSource}
+        />
+      </List>
+    )
+  }
+}
